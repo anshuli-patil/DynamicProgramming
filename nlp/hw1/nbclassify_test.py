d2eaec728b3595089fd30f27b26ca87edc40ba73
Anshuli Patil <anshulip@usc.edu>
2017-01-25 18:09:08 -0800

    calculating f1 score based on confusion matrix

--- nlp/hw1/nbclassify_test.py
+++ nlp/hw1/nbclassify_test.py
@@ -1,11 +1,11 @@
 from nlp.hw1.data_tokenizer import DataTokenizer
-
+import numpy
 
 labels_filename = 'train-labels.txt'
 output_filename = 'nboutput.txt'
 
 
-def nb_test( path):
+def nb_test(path):
     absolute_path = path + labels_filename
     label_tokenizer = DataTokenizer(absolute_path)
     labels_dict = label_tokenizer.read_labels()
@@ -14,22 +14,11 @@ def nb_test( path):
     output_tokenizer = DataTokenizer(absolute_path)
     output_dict = output_tokenizer.read_labels()
 
-    false_positives = 0
-    true_positives = 0
-    false_negatives = 0
-    true_negatives = 0
-
-    correct = 0
-    wrong = 0
-    # Compare results from nboutput.txt
-    for key in output_dict:
-        value = output_dict[key]
-        actual_value = labels_dict[key]
-        if value == actual_value:
-            correct += 1
-        else:
-            wrong += 1
-    print(correct, wrong)
+    avg_f1 = numpy.mean([get_feature_f1(output_dict, labels_dict, 'positive', 1),
+                         get_feature_f1(output_dict, labels_dict, 'negative', 1),
+                         get_feature_f1(output_dict, labels_dict, 'truthful', 0),
+                         get_feature_f1(output_dict, labels_dict, 'deceptive', 0)])
+    print(avg_f1)
 
 
 def calculate_precision(true_positives, false_positives):
@@ -44,5 +33,27 @@ def calculate_f1(true_positives, false_positives, false_negatives):
     precision = calculate_precision(true_positives, false_positives)
     recall = calculate_precision(true_positives, false_negatives)
     return 2 * precision * recall / (precision + recall)
+
+
+def get_feature_f1(output_dict, labels_dict, feature, feature_type):
+    false_positives = 0
+    true_positives = 0
+    false_negatives = 0
+    true_negatives = 0
+
+    # Compare results from nboutput.txt
+    for key in output_dict:
+        predicted_value = output_dict[key]
+        actual_value = labels_dict[key]
+        if predicted_value[feature_type] == feature and actual_value[feature_type] == feature:
+            true_positives += 1
+        elif predicted_value[feature_type] == feature and actual_value[feature_type] != feature:
+            false_positives += 1
+        elif predicted_value[feature_type] != feature and actual_value[feature_type] == feature:
+            false_negatives += 1
+        elif predicted_value[feature_type] != feature and actual_value[feature_type] != feature:
+            true_negatives += 1
+    return (calculate_f1(true_positives, false_positives, false_negatives))
+
 
 nb_test('/Users/anshulip/PycharmProjects/DynamicProg/nlp/hw1/')
